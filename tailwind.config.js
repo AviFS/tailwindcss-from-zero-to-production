492a3a2dd63695d47d660b0061a4d5c2fccf9739
AviFS <avifsternlieb@gmail.com>
2021-08-08 13:12:58 -0700

    Committing all other minor changes

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./04-hover-focus-and-other-states/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
